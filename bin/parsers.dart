16a51ad7e9367e3583f287834110df4f45107720
perymerdeka <tkjasikk@gmail.com>
2021-10-12 22:12:54 +0700

    Updating Sample Parser

--- bin/parsers.dart
+++ bin/parsers.dart
@@ -2,11 +2,10 @@ import 'dart:io';
 import 'package:html/dom.dart';
 import 'package:html/parser.dart' as html_parser;
 
-
 /// creating parser function like beautifulSoup using html5lib parser
 Future<Document> parser(String files) async {
   final data = File(files);
-  
+
   var content = await data.readAsString();
   Document html_doc = html_parser.parse(content);
   return html_doc;
@@ -14,10 +13,22 @@ Future<Document> parser(String files) async {
 
 // parsing process
 main(List<String> args) async {
+  getAllItem();
+}
+
+getAllItem() async {
   var soup = await parser('panduan1.html');
-  final contents = soup.querySelectorAll('p');
-  for (final content in contents) {
+  final title = soup.querySelector('p.title')!.text;
+  final subtitle = soup.querySelector('p.subtitle')!.text;
+  final class_content = soup.querySelectorAll('p[class^="c"]');
+
+  /// get all data on p tag
+  for (final content in class_content) {
+    if ('c12 title' == content.className) {
+      continue;
+    } else if ('c11 subtitle' == content.className) {
+      continue;
+    }
     print(content.text);
   }
-
 }
